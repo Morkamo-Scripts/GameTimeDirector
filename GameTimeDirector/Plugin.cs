cb7057386016a9eca2c463940360b99a0a307411
Кирилл <131317714+Morkamo@users.noreply.github.com>
2026-01-01 23:05:19 +0900

    Initial commit

---
+++ GameTimeDirector/Plugin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Exiled.API.Features;
+
+namespace GameTimeDirector
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Name => nameof(GameTimeDirector);
+        public override string Prefix => Name;
+        public override string Author => "Morkamo";
+        public override Version Version => new Version(1, 0, 0);
+        public override Version RequiredExiledVersion => new Version(9, 12, 1);
+
+        public static Plugin Instance { get; private set; }
+
+        public override void OnEnabled()
+        {
+            Instance = this;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Instance = null;
+            base.OnDisabled();
+        }
+    }
+}
